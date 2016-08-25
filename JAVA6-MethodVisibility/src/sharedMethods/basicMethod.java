f134f3d2dd6b0b40d96332dbeb8f094471dce2aa
TestSheepNZ <mike_talks@hotmail.com>
2016-08-25 21:16:27 +1200

    Link

--- JAVA6-MethodVisibility/src/sharedMethods/basicMethod.java
+++ JAVA6-MethodVisibility/src/sharedMethods/basicMethod.java
@@ -1,3 +1,15 @@
+/*
+ * This program is part of my learning Java series
+ * This one looks at use of methods split over classes
+ * 
+ * Find the blog here,
+ * http://testsheepnz.blogspot.co.nz/2016/08/java-6-avoiding-tangled-code-with.html
+ * 
+ * For more information - please reread.
+ * 
+ * Mike Talks, Aug 2016
+ */
+
 package sharedMethods;
 
 public class basicMethod {
